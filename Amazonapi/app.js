dce93462075322ea34a98ca399348db311863cc6
Vigykshit katoch <katoch.vigy96@gmail.com>
2023-06-15 01:37:49 +0530

    test

--- Amazonapi/app.js
+++ Amazonapi/app.js
@@ -2,11 +2,12 @@ let express = require('express');
 let app = express();
 let port = 9120;
 let {dbConnect,db} = require('./controller/dbController');
+const { LOADIPHLPAPI } = require('dns');
 app.get('/',(req,res)=>{
     res.send('we are using amazonapi')
 })
 
-app.get('/category',(req,res)=>{
+app.get('/category',(req,res)=>{LOADIPHLPAPI
     res.send('we are using amazonapi now use Amazon')
 })
 
